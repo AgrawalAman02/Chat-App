9010210d63b23f263d2874ad66bd0d6c593a8d9e
AgrawalAman02 <amanhzb2003@gmail.com>
2024-10-30 23:15:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+// we will do all io operation for Socket.io
+io.on("connection", (socket)=>{
+    socket.on("user-message",(message)=>{
+        io.emit("message",message);
+    });
+});
+
+
+
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req,res)=>{
+    return res.sendFile("/public/index.html");
+});
+
+server.listen(7777 , ()=>{
+    console.clear();
+    console.log("Server is successfully listening on port...");
+})
